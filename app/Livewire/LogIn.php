d84cd48557feb6637eab46c578788aef40788220
tomassue <tomabella64@gmail.com>
2023-12-08 14:07:38 +0800

    with livewire

---
+++ app/Livewire/LogIn.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class LogIn extends Component
+{
+    public function render()
+    {
+        return view('livewire.log-in');
+    }
+}
